8d07fe26da8d5535043a294e0490fd33525e5342
Sivathoughts <sivamurugan.win@gmail.com>
2026-06-28 08:31:07 -0700

    My first commit

---
+++ tests/api_tests.spec.js
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test';
+
+//DLETE OPERATION
+
+test('API DELETE Request', async({request}) => {
+
+const response = await request.delete('https://jsonplaceholder.typicode.com/users/1');
+expect(response.status()).toBe(200);
+
+}
+)
+
+//PUT OPERATION
+
+test('API PUT Request', async({request}) => {
+
+    const response=await request.put('https://jsonplaceholder.typicode.com/posts/1',{
+        data: {
+            "id": 1,
+            "title": "Updated Title",
+            "body": "Updated Body",
+            "userId": 1
+}
+        })  
+expect(response.status()).toBe(200);
+
+const text=await response.text();
+expect(text).toContain('Updated Body');
+
+console.log(await response.json());
+    })
+
+//POST OPERATION
+
+test('API POST Request', async({request}) => {
+
+    const response=await request.post('https://jsonplaceholder.typicode.com/posts',{
+        data: {
+            "title": "Playwright API Testing",
+            "body": "Learning POST API",
+            "userId": 1
+        }  
+    })
+
+expect(response.status()).toBe(201);
+
+const text=await response.text();
+expect(text).toContain('Learning POST API');
+
+console.log(await response.json());
+})   
+
+//GET OPERATION
+         
+test('API GET Request', async({request}) => {
+
+const response = await request.get('https://jsonplaceholder.typicode.com/users/1');
+expect(response.status()).toBe(200);
+
+const text=await response.text();
+expect(text).toContain('Graham');
+
+console.log(await response.json());
+
+}
+)
